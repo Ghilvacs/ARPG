5faf3d8b77fad13c880ebbd3eb628585d03272e2
Ghilvacs <tudor.ghilvacs@gmail.com>
2024-11-25 14:02:53 +0200

    Added stamina consumption to dash ability

--- Scripts/Enemy/EnemyFollow.gd
+++ Scripts/Enemy/EnemyFollow.gd
@@ -2,7 +2,7 @@ extends State
 class_name EnemyFollow
 
 @export var enemy: CharacterBody2D
-@export var move_speed := 180.0
+@export var move_speed := 200.0
 var player: CharacterBody2D
 
 func enter() -> void:
