7caec55e24c7f20df3c49a971f0d5e2b1d9ae1af
Ghilvacs <tudor.ghilvacs@gmail.com>
2025-04-23 10:26:43 +0300

    Solved the sprite.flip_h bug

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -36,6 +36,9 @@ func _ready() -> void:
 	player_state_machine.initialize(self)
 
 func _physics_process(delta: float) -> void:
+	player_state_machine.current_state.physics_update(delta)
+	print("Mouse at:", get_global_mouse_position())
+	print("BladePivot rotated to:", blade_one_attack_point.rotation)
 	if dead:
 		point_light.visible = false
 		if animation_player.is_playing() && animation_player.current_animation != "death":
@@ -48,20 +51,26 @@ func _physics_process(delta: float) -> void:
 		current_stamina = 100.0
 		timer_stamina_regen.stop()
 	
-	mouse_position = get_local_mouse_position()
-	blade_one_attack_point.look_at(get_global_mouse_position())
-	point_light.look_at(get_global_mouse_position())
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	if get_global_mouse_position().x > sprite.global_position.x && !isAttacking:
-		sprite.flip_h = false
-	elif get_global_mouse_position().x < sprite.global_position.x && !isAttacking:
-		sprite.flip_h = true
+	update_facing()
 	
 	move_and_slide()
 
 func update_animation(state: String) -> void:
 	animation_player.play(state)
+
+func update_facing() -> void:
+	mouse_position = get_local_mouse_position()
+	
+#	if get_global_mouse_position().x > sprite.global_position.x && !isAttacking:
+#		sprite.flip_h = false
+#	elif get_global_mouse_position().x < sprite.global_position.x && !isAttacking:
+#		sprite.flip_h = true
+	if !isAttacking:
+		blade_one_attack_point.look_at(get_global_mouse_position())
+		point_light.look_at(get_global_mouse_position())
+		sprite.flip_h = mouse_position.x < 0
 
 func death() -> void:
 	collision_shape.disabled = true
